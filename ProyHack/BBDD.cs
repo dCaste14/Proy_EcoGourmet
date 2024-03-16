cb6155f39d57bb3a00297e4684e974dbd6ee0920
Javi <javi22020>
2024-03-16 14:00:23 +0100

    BBDD

--- ProyHack/BBDD.cs
+++ ProyHack/BBDD.cs
@@ -3,23 +3,27 @@ using MySql.Data.MySqlClient;
 string connString = "server=localhost;port=3306;database=ecogourmet;uid=root;pwd=tu_contraseña;";
 using(var connection = new MySqlConnection(connString))
 {
-    
-    try
+    class BBDD
     {
-        connection.Open();
-        Console.WriteLine("Conexión exitosa");
-    }
-    catch (MySqlException e)
-    {
-        Console.WriteLine("Error al conectar: " + e.Message);
-    }
-    bool usuario_existe(string usuario)
-    {
-        string query = "SELECT COUNT(*) FROM usuarios WHERE usuario = @usuario";
-        using(var cmd = new MySqlCommand(query, connection))
+        try
         {
-            cmd.Parameters.AddWithValue("@usuario", usuario);
-            return (int)cmd.ExecuteScalar() > 0;
+            connection.Open();
+            Console.WriteLine("Conexión exitosa");
+        }
+        catch (MySqlException e)
+        {
+            Console.WriteLine("Error al conectar: " + e.Message);
+        }
+        bool usuario_existe(string usuario)
+        {
+            string query = "SELECT COUNT(*) FROM usuarios WHERE usuario = @usuario";
+            using(var cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                return (int)cmd.ExecuteScalar() > 0;
+            }
         }
     }
+    
+    
 }
