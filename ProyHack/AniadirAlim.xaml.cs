249fa8d1a7c2b7a421b4c0a413f9dd8761e85a31
luisesm <103846301+luisesm@users.noreply.github.com>
2024-05-07 18:32:59 +0200

    Añadir alimento

---
+++ ProyHack/AniadirAlim.xaml.cs
@@ -0,0 +1,12 @@
+namespace ProyHack;
+using System;
+using System.Windows;
+public partial class AniadirAlim : ContentPage
+{
+	public AniadirAlim()
+	{
+		InitializeComponent();
+        DatePicker FechaCad = new DatePicker();
+      
+    }
+}
