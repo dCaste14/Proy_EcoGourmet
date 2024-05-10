a794494188244472779714317209f409fac385e6
luisesm <103846301+luisesm@users.noreply.github.com>
2024-05-10 17:33:43 +0200

    Añadir terminado

--- ProyHack/AniadirAlim.xaml.cs
+++ ProyHack/AniadirAlim.xaml.cs
@@ -1,17 +1,35 @@
 namespace ProyHack;
 using System;
 using System.Windows;
+
 public partial class AniadirAlim : ContentPage
 {
-	public AniadirAlim()
+    public string SelectedElement { get; set; }
+    public DateTime SelectedDate { get; set; }
+
+    public AniadirAlim()
 	{
 		InitializeComponent();
         DatePicker FechaCad = new DatePicker();
       
     }
+<<<<<<< Updated upstream
 
     private async void OnNavegar1Cliked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new LandingPage());
+=======
+    private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = sender as Picker;
+        SelectedElement = picker.SelectedItem.ToString();
+        // Aquí puedes usar SelectedElement para lo que necesites
+    }
+
+    private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        SelectedDate = e.NewDate;
+        // Aquí puedes usar SelectedDate para lo que necesites
+>>>>>>> Stashed changes
     }
 }
